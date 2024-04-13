d979145f1add0241eb6ff7f4e4a2e2dd53a52d04
amahman <163727300+amahman@users.noreply.github.com>
2024-04-13 11:03:00 +0800

    Update delv2ray.sh

--- menu/delv2ray.sh
+++ menu/delv2ray.sh
@@ -60,6 +60,6 @@ echo "Username  : $user"
 
 echo "Expired   : $exp"
 echo  ============================================================
-echo           ANGUN Vpn Premium
+echo           CROT Vpn Premium
 echo ============================================================
 
